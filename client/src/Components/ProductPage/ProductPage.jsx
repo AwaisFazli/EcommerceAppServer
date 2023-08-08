e3b86391e52d093d6e660620d38a6ac6ff3233d6
Awaisfazli <awaisfazli947@gmail.com>
2023-08-08 06:47:59 +0500

    Redefining the api routes

--- client/src/Components/ProductPage/ProductPage.jsx
+++ client/src/Components/ProductPage/ProductPage.jsx
@@ -19,7 +19,7 @@ const ProductsPage = () => {
         console.log(response.data);
       })
       .catch((error) => {
-        console.error("Error fetching products:", error);
+        console.error("Error fetching products:", error.message);
       });
   }, []);
 
